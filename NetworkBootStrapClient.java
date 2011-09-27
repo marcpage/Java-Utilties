d896162677236da1f8bd38d63990c09a579d9fbc
Marc Page <marcallenpage@gmail.com>
2011-09-27 12:09:58 -0500

    Documentation added to NetworkBootStrap*

--- NetworkBootStrapClient.java
+++ NetworkBootStrapClient.java
@@ -7,29 +7,60 @@ import java.io.DataOutputStream;
 import java.lang.reflect.Method;
 import java.lang.reflect.InvocationTargetException;
 
-/**
-	Protocol
+/** Minimal class to load the rest of a project across the network.
+	<p><b>Class Set Revisions</b><br>
+	The <code>revision</code> is the revision, or version, or classes to load from the server.
+	This is to support multiple simultaneous revisions used by different clients. When a new revision
+	is available, a client can detect that and decide to upgrade. If it is in the middle of an
+	uninterruptable operation, it can keep using the old revision until it is ready to upgrade.
+	<p><b>Protocol</b>
 	<ul>
 		<li><b>Send</b>:	"revision?"<br>
-			<b>Receive</b>:	<revision Int>
+			<b>Receive</b>:	(revision Int)
 
-		<li><b>Send</b>:	"NetworkBootStrap"<protocol version Int><requested revision Int>
-			<b>Receive</b>:	<revision assigned Int>
+		<li><b>Send</b>:	"NetworkBootStrap"(protocol version Int)(requested revision Int)<br>
+			<b>Receive</b>:	(revision assigned Int)
 
-		<li><b>Send</b>:	"load""<name>"<revision Int>
-			<b>Receive</b>:	<class size in bytes Int>[<class bytes>]
+		<li><b>Send</b>:	"load""(name)"(revision Int)<br>
+			<b>Receive</b>:	(class size in bytes Int)[(class bytes)]
 
-		<li><b>Send</b>:	"quit"
+		<li><b>Send</b>:	"quit"<br>
 			<b>Receive</b>:	"quit"
 	</ul>
+	<b>TODO</b>
+	<ul>
+		<li>Look up parameters in the manifest so they don't need to be passed on the command line.
+	</ul>
+	@see NetworkBootStrapServer
 */
 public class NetworkBootStrapClient extends ClassLoader {
+	/** Initialize the connection to a NetworkBootStrapServer.
+		@param server				The server DNS name or IP address as a string.
+		@param port					The server port to connect to.
+		@param revisionRequested	The class set revision.
+										0 == latest,
+										negative == undefined behavior,
+										positive == revision number.
+		@throws IOException			On io error
+	*/
 	public NetworkBootStrapClient(String server, int port, int revisionRequested) throws IOException {
 		_init(server, port, revisionRequested);
 	}
+	/** Find a class of the given name.
+		Override of ClassLoader implementation.
+		@param name	The name of the class to load.
+		@return		The class requested.
+		@throws ClassNotFoundException	If the class is not a System class
+											or found in the current revision.
+	*/
 	public Class findClass(String name) throws ClassNotFoundException {
 		return _getClass(name);
 	}
+	/** Requests a disconnect from the server.
+		@return	<code>true</code> if the server responded to the quit <code>false</code> if the server
+						rejected the quit.
+		@throws IOException			On io error
+	*/
 	public synchronized boolean shutdown() throws IOException {
 		String	quit;
 
@@ -37,6 +68,18 @@ public class NetworkBootStrapClient extends ClassLoader {
 		quit= _in.readUTF();
 		return quit.equals("quit");
 	}
+	/** Gets the class set revision being used.
+		@return	The revision number of the actual class set we are using (not just what we requested).
+	*/
+	public int revision() {
+		return _revision;
+	}
+	/** Determines if we are using the most recent revision of the classes.
+		Meant to be used to determine if we should restart to use a newer revision of the classes.
+		@return	<code>true</code> if there is a newer revision of the classes than the version
+					we are currently using. <code>false</code> if we are up to date.
+		@throws IOException			On io error
+	*/
 	public synchronized boolean newVersionAvailable() throws IOException {
 		if(_newVersionAvailable) {
 			int	newest;
@@ -47,6 +90,11 @@ public class NetworkBootStrapClient extends ClassLoader {
 		}
 		return _newVersionAvailable;
 	}
+	/** Starts the connection to the NetworkBootStrapServer and sends the hello message.
+		@param server				DNS or IP string of the server
+		@param port					port to connect to on the server
+		@param revisionRequested	The revision number of the class set to use, or 0 for latest.
+	*/
 	private void _init(String server, int port, int revisionRequested) throws IOException {
 		_server= server;
 		_port= port;
@@ -63,6 +111,14 @@ public class NetworkBootStrapClient extends ClassLoader {
 		_out.writeInt(revisionRequested);
 		_revision= _in.readInt();
 	}
+	/** Lookup the class name in all locations.
+		First looks in the HashMap cache of classes already seen.
+		Then looks in the System for the class.
+		Finally loads the class from the NetworkBootStrapServer.
+		@param name	The name of the class to load
+		@return		The found class
+		@throws ClassNotFoundException	If the class is not found.
+	*/
 	private synchronized Class _getClass(String name) throws ClassNotFoundException {
 		Class<?>	result= _classes.get(name);
 
@@ -76,7 +132,11 @@ public class NetworkBootStrapClient extends ClassLoader {
 		}
 		return _loadClassFromServer(name);
 	}
-
+	/** Loads the class from the NetworkBootStrapServer.
+		@param name	The name of the class to load
+		@return		The found class
+		@throws ClassNotFoundException	If the class is not found.
+	*/
 	private Class _loadClassFromServer(String name) throws ClassNotFoundException {
 		int		classSize;
 		byte[]	data;
@@ -99,16 +159,42 @@ public class NetworkBootStrapClient extends ClassLoader {
 		}
 		return result;
 	}
+	/** The protocol version this class implements. */
 	static private final int			kNetworkBootStrapProtocolVersion= 1;
+	/** The revision of the class set that we are currently using. */
 	private int							_revision;
+	/** The server we're connected to. */
 	private String						_server;
+	/** The port on the server we're connected to. */
 	private int							_port;
+	/** The socket we're connected on. */
 	private Socket						_connection;
+	/** The cache of classes we've already loaded from _server. */
     private HashMap<String,Class<?>>	_classes;
+    /** The input stream from _server. */
     private DataInputStream				_in;
+    /** The output stream to _server. */
     private DataOutputStream			_out;
+    /** Once we determine we're out of date, we just cache that and don't ask again. */
     private boolean						_newVersionAvailable;
 
+	/** Starts a connection to a NetworkBootStrapServer and starts a class from it.
+		@param args	The arguments are:<ul>
+						<li><b>-server</b> <i>Optional</i> The DNS or IP address of the NetworkBootStrapServer.
+											Defaults to <i>localhost</i>.
+						<li><b>-port</b> <i>Optional</i> The port to connect to on the NetworkBootStrapServer.
+											Defaults to <i>6277</i> (telephone keypad NBSP).
+						<li><b>-class</b> <i>Optional</i> The full name of the class to load from NetworkBootStrapServer
+											as the launcher class.
+											Defaults to <i>Start</i>.
+						<li><b>-method</b> <i>Optional</i> The name of the static method to call on the launcher class.
+											The prototype is: <code>int start(NetworkBootStrapClient,String...)</code>
+											Defaults to <i>start</i>.
+						<li><b>-revision</b> <i>Optional</i> The revision of the class set to use.
+											Defaults to <i>0</i> (latest).
+					</ul>
+					Any other arguments are passed to the static class start method.
+	*/
 	public static void main(String... args) {
 		String					server= "localhost";
 		int						port= 6277;		// keypad NBSP
--- NetworkBootStrapServer.java
+++ NetworkBootStrapServer.java
@@ -6,18 +6,64 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.ByteArrayOutputStream;
 
+/** Serve up Java classes to a client.
+	It is recommended that you implement your own custom class loader to handle revisions, etc.
+	@see NetworkBootStrapClient
+*/
 public class NetworkBootStrapServer implements SocketServer.Handler {
+	/** Used to find the classes to serve up to the client.
+	*/
 	public interface ClassLoader {
+		/** Gets the latest class set revision number.
+			@return	Return the latest fully installed class set.
+			@see NetworkBootStrapClient
+		*/
 		public int latestRevision();
+		/** Loads the class data from a class set revision.
+			@param revision	The class set revision to load.
+			@param name		The name of the class to load.
+			@return			The contents of the class.
+			@throws IOException	on io error.
+		*/
 		public byte[] load(int revision, String name) throws IOException;
+		/** How to handle logging of events.
+			@param level	0 = vital information, 100 = trivial information
+			@param message	The message to log
+		*/
+		public void log(int level, String message);
+		/** Log that an exception occurred.
+			@param exception	The exception the occurred.
+		*/
+		public void log(Exception exception);
 	}
+	/** An example class loader.
+		Will probably not work for inner classes or packaged classes.
+		Does not look in jars or zips.
+		Does not handle multiple class set revisions.
+	*/
 	public static class SingleDirectoryClassLoader implements ClassLoader {
+		/** Loads classes from a given directory.
+			@param directory	The directory to load the classes from.
+		*/
 		public SingleDirectoryClassLoader(File directory) {
 			_directory= directory;
 		}
+		/** The latest revision is always 0.
+			@return 0
+		*/
 		public int latestRevision() {
 			return 0;
 		}
+		/** Loads the bytecode for a class.
+			First tries replacing dot (.) with dollar ($) and appending .class and loading that file.
+			Then if that doesn't exist, tries turning dot (.) separated items into directories.
+			Inner classes in packages will definitely not work.
+			If the file is found, loads the contents and returns them.
+			@param revision	Must be 0.
+			@param name		The name of the class.
+			@return			The bytecode for the requested class.
+			@throws IOException	on io error.
+		*/
 		public byte[] load(int revision, String name) throws IOException {
 			File					file;
 			FileInputStream			in;
@@ -28,7 +74,7 @@ public class NetworkBootStrapServer implements SocketServer.Handler {
 			if(0 != revision) {
 				return new byte[0]; // requesting an unknown revision
 			}
-			file= new File(_directory, name+".class");
+			file= new File(_directory, name.replace(".","$")+".class");
 			if(!file.isFile()) {
 				String[]	elements= name.split(".");
 
@@ -57,13 +103,39 @@ public class NetworkBootStrapServer implements SocketServer.Handler {
 			out.close();
 			return out.toByteArray();
 		}
+		/** Prints the message and level to stdout.
+			@param level	0 = vital information, 100 = trivial information
+			@param message	The message to log
+		*/
+		public void log(int level, String message) {
+			System.out.println(""+level+": "+message);
+		}
+		/** Calls printStackTrace() on the exception.
+			@param exception	The exception the occurred.
+			@see java.lang.Exception.printStackTrace
+		*/
+		public void log(Exception exception) {
+			exception.printStackTrace();
+		}
+		/** The directory to search for classes. */
 		private File	_directory;
 	}
+	/** Start a new server.
+		@param port		The port to listen on.
+		@param loader	The mechanism to find classes in class set revisions.
+		@throws IOException	on io error.
+	*/
 	public NetworkBootStrapServer(int port, ClassLoader loader) throws IOException {
 		_port= port;
 		_loader= loader;
 		_server= new SocketServer(_port, this);
 	}
+	/** Handles an incoming connection.
+		Loops on reading commands and responding to them until quit is sent or an exception.
+		@param server		Should be the same as <code>_server</code>.
+		@param connection	The connection to the client.
+		@throws IOException on io error or if the protocol of the client is not compatible.
+	*/
 	public void handle(SocketServer server, Socket connection) throws IOException {
 		DataInputStream		in= new DataInputStream(connection.getInputStream());
 		DataOutputStream	out= new DataOutputStream(connection.getOutputStream());
@@ -95,16 +167,37 @@ public class NetworkBootStrapServer implements SocketServer.Handler {
 			}
 		}
 	}
+	/** Log messages.
+		Forward log messages to the loader.
+		@param level	0 = vital information, 100 = trivial information
+		@param message	The message to log
+	*/
 	public void log(int level, String message) {
-		System.out.println(""+level+": "+message);
+		_loader.log(level, message);
 	}
+	/** Log that an exception occurred.
+		Forward log messages to the loader.
+		@param exception	The exception the occurred.
+	*/
 	public void log(Exception exception) {
-		exception.printStackTrace();
+		_loader.log(exception);
 	}
+	/** The protocol version. Must match what the client reports. */
 	static private final int		kNetworkBootStrapProtocolVersion= 1;
+	/** The port to listen on. */
 	private int						_port;
+	/** The server that is handling the connections. */
 	private SocketServer			_server;
+	/** The class loading mechanism. */
 	private ClassLoader				_loader;
+	/** Test. Serves up classes in one directory (with no revisions).
+		@param args	The arguments are:<ul>
+						<li><b>-port</b> <i>Optional</i> The port to connect to listen on.
+											Defaults to <i>6277</i> (telephone keypad NBSP).
+					</ul>
+					Only one other argument should be passed, the directory that contains the classes
+						to load.
+	*/
 	public static void main(String... args) {
 		int						port= 6277;		// keypad NBSP
 		java.util.ArrayList<String>		argsList= new java.util.ArrayList<String>(args.length);
