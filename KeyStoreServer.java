0584666bb5133142bc2e5da07eb81829568b1a1c
Marc Page <marcallenpage@gmail.com>
2012-01-13 10:17:43 -0600

    Working on KeyStoreServer

--- KeyStoreServer.java
+++ KeyStoreServer.java
@@ -2,17 +2,33 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-class KeyStoreServer implements HTTPServer.Handler {
+public class KeyStoreServer implements HTTPServer.Handler {
 	public boolean handle(InputStream in, OutputStream out, HTTPServer.KeyValuesMap headers, HTTPServer.KeyValuesMap query, HTTPServer.CookieJar cookies) throws IOException {
-		String	path= headers.firstValue("PATH", "/");
+		String		path= headers.firstValue("PATH", "/");
+		String[]	parts= path.split("/", 3);
 
-		HTTPServer.writeResponse(out, "1.1", 200, "OK");
-		HTTPServer.writeHeader(out, "Content-Type", "text/html");
-		HTTPServer.finishHeaders(out);
-		HTTPServer.write(out, "<html><head><title>Test</title></head><body><h1><center>Test</center></h1>\r\n");
-		HTTPServer.write(out, path);
-		HTTPServer.write(out, "</body></html>\r\n");
-		HTTPServer.CloseConnection(headers);
+		if( (null != parts) && (parts.length == 3) && (parts[1].equalsIgnoreCase("key")) ) {
+			String	key=parts[2];
+
+			// get or put a key
+			HTTPServer.writeResponse(out, "1.1", 200, "OK");
+			HTTPServer.writeHeader(out, "Content-Type", "text/html");
+			HTTPServer.finishHeaders(out);
+			HTTPServer.write(out, "<html><head><title>Test</title></head><body><h1><center>Test</center></h1>\r\n");
+			HTTPServer.write(out, "key=<b>"+key+"</b><br/>");
+			HTTPServer.write(out, "METHOD=<b>"+headers.getProperty("METHOD", "[UNKNOWN]")+"</b><br/>");
+			HTTPServer.write(out, "</body></html>\r\n");
+			HTTPServer.CloseConnection(headers);
+		} else {
+			HTTPServer.writeResponse(out, "1.1", 200, "OK");
+			HTTPServer.writeHeader(out, "Content-Type", "text/html");
+			HTTPServer.finishHeaders(out);
+			HTTPServer.write(out, "<html><head><title>Test</title></head><body><h1><center>Test</center></h1>\r\n");
+			HTTPServer.write(out, "parts="+parts.length+" 1st="+parts[1]+"<br/>");
+			HTTPServer.write(out, "METHOD="+headers.getProperty("METHOD", "[UNKNOWN]"));
+			HTTPServer.write(out, "</body></html>\r\n");
+			HTTPServer.CloseConnection(headers);
+		}
 		return true;
 	}
 	/** Handles logging exceptions
